a19f5c886e9499b0d5c7305c393f818dd1c01860
SADMAN30102001SAKIB <SADMAN30102001SAKIB>
2024-03-06 08:53:25 +0600

    v1.0

--- script.js
+++ script.js
@@ -13,23 +13,34 @@ document
       questionsCount: formData.get("questionsCount"),
     };
 
-    fetch("https://sadman30102001.pythonanywhere.com/generate-pdf", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "output.pdf";
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
+    if (
+      parseInt(data.rollDigit) >= 1 &&
+      parseInt(data.rollDigit) <= 11 &&
+      parseInt(data.setCount) >= 1 &&
+      parseInt(data.setCount) <= 4 &&
+      parseInt(data.questionsCount) >= 1 &&
+      parseInt(data.questionsCount) <= 35
+    ) {
+      fetch("https://sadman30102001.pythonanywhere.com/generate-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       })
-      .catch((error) => console.log("Error:", error));
+        .then((response) => response.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "output.pdf";
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        })
+        .catch((error) => console.log("Error:", error));
+    } else {
+      alert("Please enter valid inputs");
+    }
   });
